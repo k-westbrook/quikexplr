98ceba1a15b8e314b348beca90fbe28dd78ad69e
k-westbrook <kcheriewestbrook@gmail.com>
2019-01-18 10:04:36 -0500

    update sequence

--- server/api/location.js
+++ server/api/location.js
@@ -17,17 +17,13 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const clientIP = req.clientInfo.ip
-    await ipstack(
-      '172.254.159.106',
-      process.env.IPSTACK_KEY,
-      (err, response) => {
-        try {
-          res.json(response)
-        } catch (error) {
-          next(err)
-        }
+    await ipstack(clientIP, process.env.IPSTACK_KEY, (err, response) => {
+      try {
+        res.json(response)
+      } catch (error) {
+        next(err)
       }
-    )
+    })
   } catch (err) {
     next(err)
   }
@@ -52,6 +48,7 @@ router.post('/cities', async (req, res, next) => {
       if (err) {
         console.log(err)
       } else {
+        console.log(body)
         const citiesList = JSON.parse(body)
 
         res.json(citiesList)
