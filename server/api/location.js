d3d1e042a5ba637ed7dc591f575280b463be16cf
k-westbrook <kcheriewestbrook@gmail.com>
2019-01-20 14:53:10 -0500

    weather add

--- server/api/location.js
+++ server/api/location.js
@@ -8,7 +8,7 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const clientIP = req.clientInfo.ip
-    await ipstack('71.190.247.98', process.env.IPSTACK_KEY, (err, response) => {
+    await ipstack(clientIP, process.env.IPSTACK_KEY, (err, response) => {
       try {
         req.session.userLocation = response
 
